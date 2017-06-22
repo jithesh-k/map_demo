b5f0dc1d8d137626cdf52ae84910d6295d82bc4b
jithesh k <jithesh.kuzhikali@emc.com>
2017-06-22 14:42:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <p className="App-intro">
+          <img src="map.gif"></img>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+//<img src={logo} className="App-logo" alt="logo" />
